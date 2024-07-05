b42635c271e10db343fffad88ecec703f6933090
MuhmmadMedhat <muhmmedmedhat0@gmail.com>
2024-07-05 12:44:23 +0300

    chore: Split code in modules

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {getMultipleChoices} from './utility'
+
 const RANDOM_IMG_ENDPOINT = 'https://dog.ceo/api/breeds/image/random';
 
 const BREEDS = [
@@ -149,33 +151,6 @@ const BREEDS = [
   'irish wolfhound',
 ];
 
-// Utility function to get a randomly selected item from an array
-function getRandomElement(array) {
-  const i = Math.floor(Math.random() * array.length);
-  return array[i];
-}
-
-// Utility function to shuffle the order of items in an array in-place
-function shuffleArray(array) {
-  return array.sort((a, b) => Math.random() - 0.5);
-}
-
-// TODO: 1
-// Given an array of possible answers, a correct answer value, and a number of choices to get,
-// return a list of that many choices, including the correct answer and others from the array
-function getMultipleChoices(n, correctAnswer, possibleChoices) {
-  // Use a while loop and the getRandomElement() function
-  const choices = [];
-  choices.push(correctAnswer);
-  while (choices.length < n) {
-    const choice = getRandomElement(possibleChoices);
-    // Make sure there are no duplicates in the array
-    if (!choices.includes(choice)) {
-      choices.push(choice);
-    }
-  }
-  return shuffleArray(choices);
-}
 
 // TODO: 2
 // Given a URL such as "https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg"
